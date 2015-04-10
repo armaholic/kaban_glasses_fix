79f4ea8954d5e2b06bb9a87158c125d68609997f
kaban <dudakov@yandex.ru>
2015-04-10 05:09:39 +0300

    Первый коммит

---
+++ config.cpp
@@ -0,0 +1,48 @@
+class CfgPatches
+{
+	class kaban_glasses_fix
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 1.01;
+		requiredAddons[] = {"CBA_A2_Main","ace_main","acex_main"};
+		version = "1.0";
+	};
+};
+
+class CfgGlasses
+{
+	class None;
+	class NoGlasses: None {scope=0;};
+	class Pro_Helmet: NoGlasses {};
+	class Pro_Helmet_g: NoGlasses {};
+	class Pro_Helmet_2: NoGlasses {};
+	class Kevlar_Helmet: NoGlasses {};
+	class Kevlar_Helmet_g: NoGlasses {};
+	class wdl_helmet: NoGlasses {};
+	class Barett: NoGlasses {};
+	class US_green_beret: NoGlasses {};
+	class US_black_beret: NoGlasses {};
+	class Barett_g: NoGlasses {};
+	class Bdu_cap: NoGlasses {};
+	class Bdu_cap_g: NoGlasses {};
+	class b_TacticalGlasses: NoGlasses {};
+	class headset: NoGlasses {};
+	class headset_g: NoGlasses {};
+	class gasmask: NoGlasses {};
+	class gasmask2: NoGlasses {};
+	class gasmask_helmet: NoGlasses {};
+	class gasmask_red: NoGlasses {};
+	class gasmask_helmet2: NoGlasses {};
+	class pilot_mask: NoGlasses {};
+	class pilot_mask_v: NoGlasses {};
+	class pilot_v: NoGlasses {};
+	class skull_cap: NoGlasses {};
+	class skull_cap2: NoGlasses {};
+	class taliban_hat_shadow: NoGlasses {};
+	class dive_mask: NoGlasses {};
+	class BlackSun_sof_w: NoGlasses {};
+	class RedSun_sof_w: NoGlasses {};
+	class NoGlasses_sof_w: NoGlasses {};
+	class goggles_winter: NoGlasses {};
+};
